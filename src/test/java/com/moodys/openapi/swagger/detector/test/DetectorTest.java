455262be383588ccd01dcfba501aa6a6dcdc9637
HenrySHENhr <shenhanrui@outlook.com>
2020-02-03 19:24:39 +0800

    Add unittest

---
+++ src/test/java/com/moodys/openapi/swagger/detector/test/DetectorTest.java
@@ -0,0 +1,27 @@
+package com.moodys.openapi.swagger.detector.test;
+
+import com.deepoove.swagger.diff.SwaggerDiff;
+import com.deepoove.swagger.diff.model.ChangedEndpoint;
+import com.deepoove.swagger.diff.model.Endpoint;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class DetectorTest {
+
+    final String SWAGGER_V2_DOC1 = "petstore_v2_1.json";
+    final String SWAGGER_V2_DOC2 = "petstore_v2_2.json";
+
+    @Test
+    public void testEqual() {
+        SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V2_DOC1, SWAGGER_V2_DOC2);
+        List<Endpoint> newEndpoints = diff.getNewEndpoints();
+        List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
+        List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
+        Assert.assertFalse(newEndpoints.isEmpty());
+        Assert.assertFalse(missingEndpoints.isEmpty());
+        Assert.assertFalse(changedEndPoints.isEmpty());
+    }
+
+}
